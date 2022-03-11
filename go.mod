031364090608a8cdc9eb164ddce22445715a8ecb
Alexis Betancourt <chonchah@gmail.com>
2022-03-11 23:04:52 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module caveri.mx/jukskani
+
+go 1.17
+
+require github.com/kidoman/embd v0.0.0-20170508013040-d3d8c0c5c68d
+
+require github.com/golang/glog v1.0.0 // indirect
---
+++ src/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"time"
+
+	"github.com/kidoman/embd"
+	_ "github.com/kidoman/embd/host/rpi" // This loads the RPi driver
+)
+
+var PIN_DHT22 = 17
+var PIN2RELES = []int{27, 22, 23, 24, 25, 16, 26, 6}
+var RELES []embd.DigitalPin
+
+func main() {
+	token := "43114589"
+	h := sha256.New()
+	h.Write([]byte(token))
+	encoded := hex.EncodeToString(h.Sum(nil))
+	fmt.Println(encoded)
+	embd.InitGPIO()
+	defer embd.CloseGPIO()
+	for _, pin := range PIN2RELES {
+		Rele, _ := embd.NewDigitalPin(pin)
+		Rele.SetDirection(embd.Out)
+		Rele.Write(embd.High)
+		RELES = append(RELES, Rele)
+	}
+	for {
+		for _, pin := range RELES {
+			pin.Write(embd.High)
+			time.Sleep(500 * time.Millisecond)
+			pin.Write(embd.Low)
+			time.Sleep(500 * time.Millisecond)
+			fmt.Print("*")
+		}
+	}
+}
