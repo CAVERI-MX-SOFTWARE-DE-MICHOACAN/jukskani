e3716fe5f71d84b1d9204d2ab786291e97476460
Alexis Betancourt <alexisbetancourt@tuhabi.mx>
2022-06-23 13:10:23 -0500

    ...

--- src/main.go
+++ src/main.go
@@ -15,6 +15,8 @@ import (
 	_ "github.com/kidoman/embd/host/rpi" // This loads the RPi driver
 )
 
+const TIME_FORMAT = "01 Jan 15:04:05"
+
 var PIN_DHT22 = "GPIO17"
 var PIN2RELES = []int{27, 22, 23, 24, 25, 16, 26, 6}
 var PIN2RELESSTR = []string{"GPIO27", "GPIO22", "GPIO23", "GPIO24", "GPIO25", "GPIO16", "GPIO26", "GPIO6"}
@@ -43,10 +45,15 @@ func lcd_print(line1 string, line2 string) {
 	fmt.Fprint(lcd, line2)
 
 }
-
+func lcdDisplayRoutine() {
+	for range time.Tick(1 * time.Second) {
+		now := time.Now().Format(TIME_FORMAT)
+		lcd_print(now, fmt.Sprintf("%.2f *C %.2f %%HR", Temperature, Humidity))
+	}
+}
 func readDHT(sensor *models.SensorDHT) {
 	for range time.Tick(5 * time.Second) {
-		now := time.Now().Format("Mon02 Jan 15:04")
+		now := time.Now().Format(TIME_FORMAT)
 		log.Print("Reading sensor...\t")
 
 		err := sensor.Read()
@@ -56,7 +63,6 @@ func readDHT(sensor *models.SensorDHT) {
 		}
 		Temperature = sensor.Temperature
 		Humidity = sensor.Humidity
-		lcd_print(now, fmt.Sprintf("%.2f *C %.2f %%HR", Temperature, Humidity))
 		log.Println(Temperature, "*C", Humidity, "%HR", "err: ", err)
 
 	}
