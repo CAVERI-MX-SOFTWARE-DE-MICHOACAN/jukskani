95f1ec5957c6dfe68dc3046c759ffd158d80f834
Alexis Betancourt <alexisbetancourt@tuhabi.mx>
2022-06-23 18:06:51 -0500

    ...

--- models/environ.go
+++ models/environ.go
@@ -1,6 +1,6 @@
 package models
 
 type Environ struct {
-	Relays    []Relay `json:"relays"`
-	SensorDHT *SensorDHT `json:SensorDHT`
+	Relays    []Relay    `json:"relays"`
+	SensorDHT *SensorDHT `json:"SensorDHT"`
 }
