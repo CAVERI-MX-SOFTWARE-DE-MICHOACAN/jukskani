267176074884bfb87272071224a834e17b11eaa2
Alexis Betancourt <alexisbetancourt@tuhabi.mx>
2022-06-24 11:53:13 -0500

    ...

--- main.go
+++ main.go
@@ -95,6 +95,7 @@ func initEnviron() *models.Environ {
 	log.Println("Leyendo environ.json...")
 	Env, err := loadEnviron()
 	log.Printf("Environ: %+v \n %+v \n", Env, Env.SensorDHT)
+	os.Exit(1)
 	sensorDHT = Env.SensorDHT
 	sensorDHT.Init()
 	if err != nil {
